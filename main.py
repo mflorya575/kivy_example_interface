c1587c408082c805b56ef8d9f48af994eeba2097
Michael Florya <mih2303@mail.ru>
2024-12-07 14:59:37 +0300

    update imports

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from docutils.nodes import title
-from kivy.lang import Builder
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.filechooser import FileChooserIconView
 from kivy.uix.popup import Popup
-from kivy.uix.scrollview import ScrollView
 from kivymd.app import MDApp
 
 from kivy.uix.stacklayout import StackLayout
@@ -15,8 +12,7 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.core.window import Window
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.button import MDIconButton, MDRectangleFlatButton, MDFlatButton
+from kivymd.uix.button import MDIconButton, MDRectangleFlatButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.label import MDLabel
 from kivymd.uix.selectioncontrol import MDCheckbox
